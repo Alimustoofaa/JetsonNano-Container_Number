d027bc991b69814b10947d57acd87bdf24eebee1
Ali Mustofa <softhouse.ali@gmail.com>
2021-04-29 16:39:28 +0700

    edit file_size

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ classes = ['container_number']
 detection_model = {
     'filename': 'model_container_number.pt',
     'url' : 'https://github.com/Alimustoofaa/JetsonNano-Container_Number/releases/download/1.0/model_container_number.pt',
-    'file_size' : 19161802
+    'file_size' : 14753191
 }
