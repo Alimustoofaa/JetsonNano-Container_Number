7f3fb874360e9afd3b182d36b9d93733820e2458
Ali Mustofa <softhouse.ali@gmail.com>
2021-04-29 14:27:25 +0700

    frist commit

---
+++ config.py
@@ -0,0 +1,10 @@
+# Config
+DIRECTORY_MODEL = 'models'
+
+classes = ['container_number']
+
+detection_model = {
+    'filename': 'model_container_number.pt',
+    'url' : 'https://github.com/Alimustoofaa/JetsonNano-Container_Number/releases/download/1.0/model_container_number.pt',
+    'file_size' : 19161802
+}
